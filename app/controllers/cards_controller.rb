e4bc10fae11a751e21e4affe7bb1199aee4280aa
Lukasz Sarnacki <lukesarnacki@gmail.com>
2017-07-06 19:42:43 +0200

    dont require user on cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,12 @@
 class CardsController < ApplicationController
   include JSONAPI::ActsAsResourceController
-  skip_before_action :require_user, only: [:index, :show, :show_relationship, :get_related_resource]
+
+  skip_before_action :require_user,
+                     only: [
+                       :index,
+                       :show,
+                       :show_relationship,
+                       :get_related_resource,
+                       :get_related_resources
+                     ]
 end
--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,5 +1,12 @@
 class DecksController < ApplicationController
   include JSONAPI::ActsAsResourceController
 
-  skip_before_action :require_user, only: [:index, :show, :show_relationship, :get_related_resource, :get_related_resources]
+  skip_before_action :require_user,
+                     only: [
+                       :index,
+                       :show,
+                       :show_relationship,
+                       :get_related_resource,
+                       :get_related_resources
+                     ]
 end
